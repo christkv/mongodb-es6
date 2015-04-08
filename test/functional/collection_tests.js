818aad958b21f02fbfae05d5d05b4d0ab86cc97f
Christian Kvalheim <christkv@gmail.com>
2015-04-08 11:23:15 +0200

    Fixed minor bug in dynamic proxies for es6

--- test/functional/collection_tests.js
+++ test/functional/collection_tests.js
@@ -10,12 +10,22 @@ exports['Should correctly insert documents'] = {
     co(function* () {
       // Connect
       var client = yield new MongoClient('mongodb://localhost:27017/test', {}).connect()
+      // Drop the collection
+      try { 
+        yield client['tests']['documents'].drop(); 
+        yield client['tests']['documents1'].drop(); 
+      } catch(err){}
       // Execute single insert
       var result = yield client['tests']['documents'].insertOne({a:1});
       // Execute multiple inserts
-      var result = yield client['tests']['documents'].insertMany([{a:2}, {a:3}]);
+      var result = yield client['tests']['documents1'].insertMany([{a:2}, {a:3}]);
+      // Query the data
+      var docs = yield client['tests']['documents'].find({}).toArray()
+      var docs1 = yield client['tests']['documents1'].find({}).toArray()
+
       // Finish up
-      test.ok(result != null);
+      test.ok(1, docs.length);
+      test.ok(2, docs1.length);
       client.close();
       test.done();
     }).catch(function(err) {
--- test/functional/db_tests.js
+++ test/functional/db_tests.js
@@ -22,3 +22,28 @@ exports['Should correctly connect to MongoDB using MongoClient ES6 promise'] = {
     });
   }
 }
+
+exports['Should correctly execute insert against multiple databases'] = {
+  metadata: { requires: { } },
+
+  // The actual test we wish to run
+  test: function(configuration, test) {
+    var MongoClient = require('../..').MongoClient;
+
+    co(function* () {
+      // Connect
+      var client = yield new MongoClient('mongodb://localhost:27017/test', {}).connect()
+      // Execute single insert
+      var result = yield client['tests']['documents'].insertOne({a:1});
+      // Execute multiple inserts
+      var result = yield client['tests']['documents'].insertMany([{a:2}, {a:3}]);
+      // Finish up
+      test.ok(result != null);
+      client.close();
+      test.done();
+    }).catch(function(err) {
+      console.log(err.stack)
+      test.done();
+    });
+  }
+}
